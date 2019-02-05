961cd4ba0d852b8d1a21e24ab8fc4c377332e51f
Grzegorz Gebala <grzegorz.gebala@yahoo.com>
2019-02-05 23:47:52 +0100

    20.7 done

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import uuid from 'uuid';
 import style from './App.css';
 import Title from '../components/Title.js';
 import TodoList from '../components/TodoList.js';
+import { hot } from 'react-hot-loader';
 
 class App extends React.Component {
     constructor(props){
@@ -47,4 +48,4 @@ class App extends React.Component {
     }
 }
 
-export default App;
+export default hot(module)(App);
